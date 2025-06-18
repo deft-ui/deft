817fb118e15525cc0969d1c4a9c5bb9be1acfde8
kason <me@kason.fun>
2025-06-18 12:20:56 +0800

    chore: update demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -10,14 +10,41 @@ impl IApp for AppImpl {
     fn init_js_engine(&mut self, _js_engine: &mut JsEngine) {
         // js_engine.enable_localstorage(env::current_exe().unwrap().parent().unwrap().join("localstorage"));
     }
+    #[cfg(desktop_platform)]
     fn create_module_loader(&mut self) -> Box<dyn JsModuleLoader + Send + Sync + 'static> {
+        use quick_js::loader::FsJsModuleLoader;
         let ml = FsJsModuleLoader::new("examples/demo-js");
+        Box::new(ml)
+    }
+
+    #[cfg(not(desktop_platform))]
+    fn create_module_loader(&mut self) -> Box<dyn JsModuleLoader + Send + Sync + 'static> {
+        use deft::loader::StaticModuleLoader;
+        let mut ml = StaticModuleLoader::new();
+        ml.add_module("index.js".to_string(), include_str!("./demo-js/index.js").to_owned());
         Box::new(ml)
     }
 }
 
-fn main() {
+fn bootstrap_app() {
     env_logger::init();
     let app = App::new(AppImpl {});
     bootstrap(app);
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    bootstrap_app();
+}
+
+
+#[cfg(target_os = "emscripten")]
+pub fn main() {
+    // Do nothing
+}
+
+#[cfg(target_os = "emscripten")]
+#[no_mangle]
+pub extern "C" fn asm_main() {
+    bootstrap_app();
 }
