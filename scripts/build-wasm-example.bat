45a65606fbd3710627e686b46d8c7cd15b12cc0e
kason <me@kason.fun>
2025-06-19 18:41:38 +0800

    chore: add ci for wasm target

--- scripts/build-wasm-example.bat
+++ scripts/build-wasm-example.bat
@@ -1,3 +1,2 @@
 @echo off
-set EMCC_CFLAGS="-s MAX_WEBGL_VERSION=2 -s MODULARIZE=1 -s EXPORT_NAME=loadDeftApp -s EXPORTED_RUNTIME_METHODS=GL,cwrap"
-cargo build --target wasm32-unknown-emscripten --no-default-features --example gallery
+cargo build --target wasm32-unknown-emscripten --no-default-features
